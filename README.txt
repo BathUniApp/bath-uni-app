6cadb926c2378224ee5313886a1bd224deb90419
Tristan Kemp <tristankemp@me.com>
2014-02-04 18:22:35 +0000

    Included Google Play Services

--- README.txt
+++ README.txt
@@ -1,3 +1,9 @@
 Group 7 Work
 
-tsk27 push test
+To get project working in ADK:
+
+Import the full project. 
+Import the google play services project
+
+If it still isn't working:
+In the ADK, right click on MainActivity, select properties, select 'Android', add the google play services library project. 
--- src/ip7/bathuniapp/MapsFragment.java
+++ src/ip7/bathuniapp/MapsFragment.java
@@ -1,42 +1,15 @@
 package ip7.bathuniapp;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
 
 public class MapsFragment extends Fragment {
-	
-	private SupportMapFragment fragment;
-	private GoogleMap map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return  inflater.inflate(R.layout.frag_maps, container, false);
     }
-    
-//    @Override
-//	public void onActivityCreated(Bundle savedInstanceState) {
-//		super.onActivityCreated(savedInstanceState);
-//		FragmentManager fm = getChildFragmentManager();
-//		fragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
-//		if (fragment == null) {
-//			fragment = SupportMapFragment.newInstance();
-//			fm.beginTransaction().replace(R.id.map_container, fragment).commit();
-//		}
-//	}
-//	
-//	@Override
-//	public void onResume() {
-//		super.onResume();
-//		if (map == null) {
-//			map = fragment.getMap();
-//			map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
-//		}
-//	}
 }
