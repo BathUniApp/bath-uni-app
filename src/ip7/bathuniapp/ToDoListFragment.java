41cd78c69223ff609d841056332b432c169191b9
Tristan Kemp <tristankemp@me.com>
2014-02-05 17:46:30 +0000

    Minor changes

--- src/ip7/bathuniapp/ToDoListFragment.java
+++ src/ip7/bathuniapp/ToDoListFragment.java
@@ -1,15 +1,18 @@
 package ip7.bathuniapp;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
 
 public class ToDoListFragment extends Fragment {
 
+	// Code to run on startup
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.frag_todolist, container, false);
+    	
+    	return  inflater.inflate(R.layout.frag_todolist, container, false);
     }
+    
 }
