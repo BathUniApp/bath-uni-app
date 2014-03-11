3009cd9b78da71cca661dcdcbab59103c536956a
LastPrime <ahollo1993@hotmail.co.uk>
2014-03-11 12:42:24 +0000

    test commit

---
+++ src/ip7/bathuniapp/Day.java
@@ -0,0 +1,15 @@
+package ip7.bathuniapp;
+
+import java.util.List;
+
+public class Day {
+
+	private String dayClassification;
+	private List<Stop> stops;
+	
+	public Day(List<Stop> theStops)
+	{
+		stops = theStops;
+	}
+	
+}
---
+++ src/ip7/bathuniapp/Stop.java
@@ -0,0 +1,16 @@
+package ip7.bathuniapp;
+
+import java.util.List;
+
+public class Stop {
+
+	private String name;
+	private String[] times;
+	
+	public Stop(String theName, String[] theTimes)
+	{
+		name = theName;
+		times = theTimes;
+	}
+	
+}
