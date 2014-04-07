246a63bce62553f441e6d8255572894fd2209bc5
Tristan Kemp <tristankemp@me.com>
2014-04-07 21:00:37 +0100

    P. much done

--- src/ip7/bathuniapp/BusesFragment.java
+++ src/ip7/bathuniapp/BusesFragment.java
@@ -21,6 +21,7 @@ public class BusesFragment extends Fragment {
     private static Map<String, BusRoute> allRoutes = new HashMap<String, BusRoute>();
 
     private Spinner routeSpinner;
+    private Spinner routeTypeSpinner;
     private Spinner stopSpinner;
 
     private TextView nextBus1;
@@ -30,11 +31,16 @@ public class BusesFragment extends Fragment {
     private TableLayout busTable;
     private TableLayout busTimes;
 
+    private String selectedRoute;
+    private String selectedRouteType;
+    private String selectedStop;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frag_buses, container, false);
         routeSpinner = (Spinner) v.findViewById(R.id.route_spinner);
+        routeTypeSpinner = (Spinner) v.findViewById(R.id.route_type_spinner);
         stopSpinner = (Spinner) v.findViewById(R.id.busstop_spinner);
 
         nextBus1 = (TextView) v.findViewById(R.id.time1);
@@ -44,6 +50,11 @@ public class BusesFragment extends Fragment {
         busTable = (TableLayout) v.findViewById(R.id.times_table);
         busTimes = (TableLayout) v.findViewById(R.id.times);
 
+        selectedRoute = "U18";
+        selectedRouteType = "MTF";
+
+        fillBusRoutes();
+
         routeSpinner
                 .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
@@ -51,7 +62,7 @@ public class BusesFragment extends Fragment {
                     public void onItemSelected(AdapterView<?> parent,
                             View view, int pos, long id) {
 
-                        // Update department spinner from appropriate array
+                        // Update spinner from appropriate array
                         if (parent.getItemAtPosition(pos).equals("U18")) {
                             setSpinnerContent(view, stopSpinner,
                                     R.array.stops_U18);
@@ -71,14 +82,75 @@ public class BusesFragment extends Fragment {
                             setSpinnerContent(view, stopSpinner,
                                     R.array.empty_array);
                         }
-                        
-                        ArrayList<String> busStops = allRoutes.get("U18MTF").getAllStops();
-                        
-                        for(int i = 0; i < busStops.size(); i++) {
-                            String stop = busStops.get(i);
-                            fillRow(stop, busTable, busTimes, allRoutes.get("U18MTF").getAllTimes(stop), i);
+
+                        selectedRoute = parent.getItemAtPosition(pos)
+                                .toString();
+
+                        // TODO: Change this to draw table function or
+                        // something?
+                        BusRoute route = allRoutes.get(selectedRouteType
+                                + selectedRoute);
+                        busTable.removeAllViews();
+                        busTimes.removeAllViews();
+
+                        if (route != null) {
+                            ArrayList<String> busStops = route.getAllStops();
+
+                            for (int i = 0; i < busStops.size(); i++) {
+                                String stop = busStops.get(i);
+                                fillRow(stop, busTable, busTimes, allRoutes
+                                        .get(selectedRouteType + selectedRoute)
+                                        .getAllTimes(stop), i);
+                            }
                         }
 
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        // Interface callback
+                    }
+                });
+
+        routeTypeSpinner
+                .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent,
+                            View view, int pos, long id) {
+
+                        // Update spinner from appropriate array
+                        if (parent.getItemAtPosition(pos)
+                                .equals("Term Weekday")) {
+                            selectedRouteType = "MTF";
+                        } else if (parent.getItemAtPosition(pos).equals(
+                                "Term Weekend")) {
+                            selectedRouteType = "SS";
+                        } else if (parent.getItemAtPosition(pos).equals(
+                                "Term Weekday")) {
+                            selectedRouteType = "MTFH";
+                        } else if (parent.getItemAtPosition(pos).equals(
+                                "Term Weekday")) {
+                            selectedRouteType = "SSH";
+                        } else {
+                            selectedRouteType = "MTF";
+                        }
+
+                        BusRoute route = allRoutes.get(selectedRouteType
+                                + selectedRoute);
+                        busTable.removeAllViews();
+                        busTimes.removeAllViews();
+
+                        if (route != null) {
+                            ArrayList<String> busStops = route.getAllStops();
+
+                            for (int i = 0; i < busStops.size(); i++) {
+                                String stop = busStops.get(i);
+                                fillRow(stop, busTable, busTimes, allRoutes
+                                        .get(selectedRouteType + selectedRoute)
+                                        .getAllTimes(stop), i);
+                            }
+                        }
                     }
 
                     @Override
@@ -93,26 +165,34 @@ public class BusesFragment extends Fragment {
                     @Override
                     public void onItemSelected(AdapterView<?> parent,
                             View view, int pos, long id) {
+                        
+                        // TODO Move -this- to a seperate thing, have it get re-filled out.
+                        
+                        selectedStop = parent.getItemAtPosition(pos).toString();
 
                         int currentTime = getCurrentTime();
                         String busStop = parent.getItemAtPosition(pos)
                                 .toString();
-                        // TODO make sure this gets all the information it
-                        // needs, not just the one spinner
-                        // TODO What happens if there are no other times? Check.
+                        // TODO If there are no other times, print something more graceful
                         Calendar calendar = GregorianCalendar.getInstance();
                         calendar.setTimeZone(TimeZone.getTimeZone("Etc/GMT-1"));
 
-                        int nextTime1 = allRoutes.get("U18MTF").getNextTime(
-                                busStop, currentTime);
-                        int nextTime2 = allRoutes.get("U18MTF").getNextTime(
-                                busStop, nextTime1 + 1);
-                        int nextTime3 = allRoutes.get("U18MTF").getNextTime(
-                                busStop, nextTime2 + 1);
+                        BusRoute route = allRoutes.get(selectedRouteType
+                                + selectedRoute);
+                        System.out.println(selectedRouteType + selectedRoute);
 
-                        nextBus1.setText(timeToString(nextTime1));
-                        nextBus2.setText(timeToString(nextTime2));
-                        nextBus3.setText(timeToString(nextTime3));
+                        if (route != null) {
+                            int nextTime1 = route.getNextTime(busStop,
+                                    currentTime);
+                            int nextTime2 = route.getNextTime(busStop,
+                                    nextTime1 + 1);
+                            int nextTime3 = route.getNextTime(busStop,
+                                    nextTime2 + 1);
+
+                            nextBus1.setText(timeToString(nextTime1));
+                            nextBus2.setText(timeToString(nextTime2));
+                            nextBus3.setText(timeToString(nextTime3));
+                        }
                     }
 
                     @Override
@@ -124,8 +204,6 @@ public class BusesFragment extends Fragment {
         SharedPreferences settings = this.getActivity().getPreferences(0);
         routeSpinner.setSelection(settings.getInt("bus", 0));
 
-        fillBusRoutes();
-        
         return v;
 
     }
@@ -165,7 +243,7 @@ public class BusesFragment extends Fragment {
 
         time.addView(busTimesRow);
     }
-    
+
     // Populates a spinner with the contents of the given array
     private void setSpinnerContent(View view, Spinner spinner, int textArrayId) {
 
@@ -183,177 +261,64 @@ public class BusesFragment extends Fragment {
     }
 
     public void fillBusRoutes() {
-        BusRoute myRoute = new BusRoute("U18", "MTF");
 
-        Map<String, Integer> offset = new HashMap<String, Integer>();
-        offset.put("Lower Oldfield Park", 0);
-        offset.put("Corn Street", 7);
-        offset.put("High Street", 11);
-        offset.put("Youth Hostel North", 17);
-        offset.put("University of Bath", 25);
-        offset.put("Youth Hostel South", 29);
-        offset.put("North Parade", 35);
+        // U18 offsets
+        Map<String, Integer> u18offset = new HashMap<String, Integer>();
+        u18offset.put("Lower Oldfield Park", 0);
+        u18offset.put("Corn Street", 7);
+        u18offset.put("High Street", 11);
+        u18offset.put("Youth Hostel North", 17);
+        u18offset.put("University of Bath", 25);
+        u18offset.put("Youth Hostel South", 29);
+        u18offset.put("North Parade", 35);
 
-        for (String stopName : offset.keySet()) {
-            int j = offset.get(stopName);
+        // Weekday U18
+        BusRoute u18mtf = new BusRoute("U18", "MTF");
+        for (String stopName : u18offset.keySet()) {
+            int j = u18offset.get(stopName);
             ArrayList<Integer> times = new ArrayList<Integer>();
             times.add(420 + j);
             times.add(430 + j);
             times.add(440 + j);
             for (int i = 0; i < 157; i++) {
-                times.add(445 + i * 5 + j);
+                times.add(448 + i * 5 + j);
             }
             for (int i = 0; i < 18; i++) {
                 times.add(1240 + i * 20 + j);
             }
-            myRoute.addStop(stopName, times);
-            allRoutes.put("U18MTF", myRoute);
+            u18mtf.addStop(stopName, times);
         }
+        allRoutes.put("MTFU18", u18mtf);
 
-        // BusRoute myRoute2 = new BusRoute("U18", "SAT");
-        // for (String stopName : offset.keySet()) {
-        // int j = offset.get(stopName);
-        // ArrayList<Integer> times = new ArrayList<Integer>();
-        //
-        // for (int i = 0; i < 40; i++) {
-        // times.add(465 + i * 20 + j);
-        // }
-        // times.add(1300 + j);
-        // times.add(1315 + j);
-        // for (int i = 0; i < 18; i++) {
-        // times.add(1315 + i * 20 + j);
-        // }
-        // myRoute2.addStop(stopName, times);
-        // allRoutes.put("U18SAT", myRoute2);
-        // }
-        //
-        // BusRoute myRoute3 = new BusRoute("U18", "SUN");
-        // for (String stopName : offset.keySet()) {
-        // int j = offset.get(stopName);
-        // ArrayList<Integer> times = new ArrayList<Integer>();
-        //
-        // for (int i = 0; i < 40; i++) {
-        // times.add(525 + i * 20 + j);
-        // }
-        // times.add(1300 + j);
-        // times.add(1315 + j);
-        // for (int i = 0; i < 9; i++) {
-        // times.add(1315 + i * 20 + j);
-        // }
-        // myRoute3.addStop(stopName, times);
-        // allRoutes.put("U18SUN", myRoute3);
-        // }
-        //
-        // BusRoute xroute1 = new BusRoute("X18", "MTF");
-        //
-        // Map<String, Integer> offset3 = new HashMap<String, Integer>();
-        // offset3.put("Lower Oldfield Park", 0);
-        // offset3.put("Bathwick Hill", 13);
-        // offset3.put("University Of Bath", 18);
-        //
-        // for (String stopName : offset3.keySet()) {
-        // int j = offset3.get(stopName);
-        // ArrayList<Integer> times = new ArrayList<Integer>();
-        //
-        // for (int i = 0; i < 5; i++) {
-        // times.add(460 + i * 15 + j);
-        // }
-        // for (int i = 0; i < 28; i++) {
-        // times.add(555 + i * 20 + j);
-        // }
-        // xroute1.addStop(stopName, times);
-        // allRoutes.put("X18MTF", xroute1);
-        // }
-        //
-        // BusRoute nextRoute = new BusRoute("18", "MTF");
-        // Map<String, Integer> offset2 = new HashMap<String, Integer>();
-        // offset2.put("University Of Bath", 0);
-        // offset2.put("Dorchester Street (1)", 13);
-        // offset2.put("Oldfield Park", 24);
-        // offset2.put("Dorchester Street (2)", 32);
-        // offset2.put("University of Bath", 50);
-        //
-        // for (String stopName : offset2.keySet()) {
-        // int j = offset2.get(stopName);
-        // ArrayList<Integer> times = new ArrayList<Integer>();
-        // // Very approximate times
-        //
-        // for (int i = 0; i < 140; i++) {
-        // times.add(409 + i * 6 + j);
-        // }
-        // for (int i = 0; i < 31; i++) {
-        // times.add(1170 + i * 15 + j);
-        // }
-        // nextRoute.addStop(stopName, times);
-        // allRoutes.put("18MTF", nextRoute);
-        // }
-        //
-        // BusRoute Route218 = new BusRoute("18", "SAT");
-        //
-        // for (String stopName : offset2.keySet()) {
-        // int j = offset2.get(stopName);
-        // ArrayList<Integer> times = new ArrayList<Integer>();
-        // // Very approximate times
-        //
-        // for (int i = 0; i < 61; i++) {
-        // times.add(240 + i * 20 + j);
-        // }
-        // Route218.addStop(stopName, times);
-        // allRoutes.put("18SAT", Route218);
-        // }
-        //
-        // BusRoute Route318 = new BusRoute("18", "SUN");
-        //
-        // for (String stopName : offset2.keySet()) {
-        // int j = offset2.get(stopName);
-        // ArrayList<Integer> times = new ArrayList<Integer>();
-        // // Very approximate times
-        //
-        // for (int i = 0; i < 30; i++) {
-        // times.add(310 + i * 30 + j);
-        // }
-        // Route318.addStop(stopName, times);
-        // allRoutes.put("18SUN", Route318);
-        // }
-        //
-        // BusRoute Route10 = new BusRoute("U10", "MTF");
-        //
-        // Map<String, Integer> offset4 = new HashMap<String, Integer>(); // 35
-        // offset4.put("Kingsway Post Office", 0);
-        // offset4.put("Moorland Road", 7);
-        // offset4.put("Lower Oldfield Park", 12);
-        // offset4.put("St Marys Church", 22);
-        // offset4.put("Youth Hostel", 27);
-        // offset4.put("University of Bath", 33);
-        //
-        // for (String stopName : offset4.keySet()) {
-        // int j = offset4.get(stopName);
-        // ArrayList<Integer> times = new ArrayList<Integer>();
-        // // Very approximate times
-        //
-        // for (int i = 0; i < 8; i++) {
-        // times.add(455 + i * 30 + j);
-        // }
-        // times.add(670 + j);
-        //
-        // for (int i = 0; i < 9; i++) {
-        // times.add(702 + i * 30 + j);
-        // }
-        //
-        // times.add(944 + j);
-        // times.add(983 + j);
-        // times.add(1018 + j);
-        // times.add(1058 + j);
-        // times.add(1098 + j);
-        // times.add(1138 + j);
-        // times.add(1182 + j);
-        // times.add(1242 + j);
-        // times.add(1302 + j);
-        // times.add(1362 + j);
-        //
-        // Route10.addStop(stopName, times);
-        // allRoutes.put("U10MTF", Route10);
-        // }
+        // Weekend U18
+        BusRoute u18ss = new BusRoute("U18", "SS");
+        for (String stopName : u18offset.keySet()) {
+            int j = u18offset.get(stopName);
+            ArrayList<Integer> times = new ArrayList<Integer>();
+            for (int i = 0; i < 81; i++) {
+                times.add(448 + i * 20 + j);
+            }
+            u18ss.addStop(stopName, times);
+        }
+        allRoutes.put("SSU18", u18ss);
+
+        // X18 Offsets
+        Map<String, Integer> x18offset = new HashMap<String, Integer>();
+        x18offset.put("Lower Oldfield Park", 0);
+        x18offset.put("University of Bath", 5);
+        x18offset.put("Bathwick hill", 9);
+
+        // Weekday X18
+        BusRoute x18mtf = new BusRoute("X18", "MTF");
+        for (String stopName : x18offset.keySet()) {
+            int j = x18offset.get(stopName);
+            ArrayList<Integer> times = new ArrayList<Integer>();
+            for (int i = 0; i < 28; i++) {
+                times.add(590 + i * 20 + j);
+            }
+            x18mtf.addStop(stopName, times);
+        }
+        allRoutes.put("MTFX18", x18mtf);
 
     }
 
